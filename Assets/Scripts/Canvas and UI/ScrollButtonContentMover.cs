d13181140e32ba9611b657fd7b14f9ebe60523ff
Yash Dushettiwar <yashdushettiwar@inucom.org>
2023-10-05 17:28:40 +0530

    Added Scrolling for Experiments Panel

---
+++ Assets/Scripts/Canvas and UI/ScrollButtonContentMover.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollButtonContentMover : MonoBehaviour
+{
+    public RectTransform buttonHolder;
+    public Button left,right;
+    Vector3 originalTransform;
+    [Range (-100,100)]
+    public float xValue = 0;
+    void Start(){originalTransform = buttonHolder.anchoredPosition;}
+    void Update(){float xClamped = Mathf.Clamp(xValue,-100,100); buttonHolder.anchoredPosition = new Vector3(xClamped,originalTransform.y,originalTransform.z);}
+    public void LeftClick(float stepsToDecrease){float xClamped = Mathf.Clamp(xValue,-100,100); xClamped -= stepsToDecrease; xValue = xClamped;}
+    public void RightClick(float stepsToIncrease){float xClamped = Mathf.Clamp(xValue,-100,100);xClamped += stepsToIncrease; xValue = xClamped;}
+}
